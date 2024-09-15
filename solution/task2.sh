e77f911913613d1a8b68800924a718e78d8d0c08
u3o <fivan5327@gmail.com>
2024-09-15 20:25:13 +0700

    test

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,3 +1,3 @@
-find . -name "foo.txt" -exec cat {} +
-find . -name "bar.txt" -exec cat {} +
-find . -name "baz.txt" -exec cat {} +
+find . -name "foo.txt" -exec cat {} \;
+find . -name "bar.txt" -exec cat {} \;
+find . -name "baz.txt" -exec cat {} \;
