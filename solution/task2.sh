cc1bf86390264320c39343334c4052424d577e90
u3o <fivan5327@gmail.com>
2024-09-15 19:40:33 +0700

    task2 upd

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,3 +1,3 @@
-find data -name "foo.txt" -exec cat {} +
-find data -name "bar.txt" -exec cat {} +
-find data -name "baz.txt" -exec cat {} +
+find . -name "foo.txt" -exec cat {} +
+find . -name "bar.txt" -exec cat {} +
+find . -name "baz.txt" -exec cat {} +
